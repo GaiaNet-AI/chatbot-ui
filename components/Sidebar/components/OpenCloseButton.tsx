f2878977e904bdba3266442a0250c76c7649fbd0
JYC0413 <juyichen0413@foxmail.com>
2024-05-04 02:14:02 +0800

    fix

--- components/Sidebar/components/OpenCloseButton.tsx
+++ components/Sidebar/components/OpenCloseButton.tsx
@@ -9,11 +9,11 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
   return (
     <>
       <button
-        className={`fixed top-5 ${
+        className={`fixed top-0 ${
           side === 'right' ? 'right-[290px]' : 'left-[290px]'
-        } z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+        } z-50 h-14 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:${
           side === 'right' ? 'right-[290px]' : 'left-[290px]'
-        } sm:h-8 sm:w-8 sm:text-neutral-700`}
+        } sm:h-[84px] sm:w-8 sm:text-neutral-700`}
         onClick={onClick}
       >
         {side === 'right' ? <IconArrowBarRight color="#989898" /> : <IconArrowBarLeft color="#989898"  />}
@@ -29,11 +29,11 @@ export const CloseSidebarButton = ({ onClick, side }: Props) => {
 export const OpenSidebarButton = ({ onClick, side }: Props) => {
   return (
     <button
-      className={`fixed top-2.5 ${
+      className={`fixed top-0 ${
         side === 'right' ? 'right-2' : 'left-2'
-      } z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:${
+      } z-50 h-14 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:${
         side === 'right' ? 'right-2' : 'left-2'
-      } sm:h-8 sm:w-8 sm:text-neutral-700`}
+      } sm:h-[84px] sm:w-8 sm:text-neutral-700`}
       onClick={onClick}
     >
       {side === 'right' ? <IconArrowBarLeft color="#989898" /> : <IconArrowBarRight color="#989898" />}
