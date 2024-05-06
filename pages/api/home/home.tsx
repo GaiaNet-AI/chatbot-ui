a15cee35d8b119f374a01ed7c7b51d1dd5c4a062
JYC0413 <juyichen0413@foxmail.com>
2024-05-06 10:06:24 +0800

    fix

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -438,7 +438,7 @@ const Home = ({
                     name="viewport"
                     content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
                 />
-                <link rel="icon" href="/favicon.png"/>
+                <link rel="icon" href="./favicon.png"/>
             </Head>
             {selectedConversation && (
                 <main
