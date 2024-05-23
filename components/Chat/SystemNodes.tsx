ae87846572ac4b1d0af0128f27fb550aebb01895
MasonTao <yingchun_tao@163.com>
2024-05-23 20:34:54 +0800

    style: update system node style

--- components/Chat/SystemNodes.tsx
+++ components/Chat/SystemNodes.tsx
@@ -1,4 +1,4 @@
-import { IconChevronDown } from '@tabler/icons-react';
+import { IconChevronDown,IconSearch } from '@tabler/icons-react';
 import Tippy from '@tippyjs/react';
 import React, {
   useCallback,
@@ -92,14 +92,14 @@ const SystemNodes: React.FC = () => {
 
   const renderSystemNodesContent = useMemo(() => {
     return (
-      <div className="flex flex-col w-[288px] max-h-[270px] bg-white border border-black rounded-lg overflow-hidden ">
-        <p className="text-black fm-SpaceGrotesk text-sm font-bold px-4 pt-4 pb-1">
+      <div className="flex flex-col w-[405px] max-h-[314px] bg-white border border-black rounded-lg overflow-hidden px-3 pt-[14px] pb-4">
+        <p className="text-black fm-SpaceGrotesk text-[14px] font-bold mb-4">
           Select a Node
         </p>
 
         {!channelIsDapp && (
           <a
-            className="text-gray-800 fm-SpaceGrotesk text-xs mb-4 text-d cursor-pointer px-4 mt-1 "
+            className="text-gray-800 text-xs cursor-pointer"
             target="_blank"
             href="https://www.gaianet.ai/"
           >
@@ -109,30 +109,31 @@ const SystemNodes: React.FC = () => {
 
         {channelIsDapp && (
           <>
-            <div className="px-4 my-1 w-full">
+            <div className="w-full relative mb-2">
+              <IconSearch size="20" color="#999999" className='absolute top-[10px] left-[9px]' />
               <input
                 type="text"
                 placeholder="Search node"
-                className="w-full h-[36px] px-2 text-xs text-[#322221] fm-SourceCodePro border border-[rgba(0,0,0,.08)] hover:border-[#696868] rounded-[8px] outline-none transition-all focus:border-[#696868] '"
+                className="w-full h-[40px] pr-2 pl-[35px] text-[12px] text-[#322221] fm-SpaceMono rounded-[8px] outline-none transition-all bg-[#F7F7F7] '"
                 onChange={handleSearch}
               />
             </div>
 
-            <div className="flex flex-col w-full flex-1  overflow-y-scroll overflow-x-hidden pb-3">
+            <div className="flex flex-col w-full flex-1  overflow-y-scroll overflow-x-hidden">
               {nodesOptions?.map((item: any) => {
                 return (
                   <div
-                    className="cursor-pointer w-full px-4 py-[10px] break-words border-b border-[rgba(0,0,0,.08)] bg-white hover:bg-[#f7f7f7] transition-all"
+                    className="cursor-pointer w-full px-1 py-[10px] break-words border-b border-[rgba(0,0,0,.08)] bg-white hover:bg-[#f7f7f7] transition-all"
                     key={item?.subdomain}
                     onClick={() =>
                       handleApiChange(`https://${item?.subdomain}`)
                     }
                   >
-                    <p className="text-[13px] text-[#322221] uppercase font-bold">
+                    <p className="text-[13px] text-[#000000] leading-[16px] font-bold">
                       {item?.subdomain || '-'}
                     </p>
-                    <p className="text-[10px] text-[#322221]  mt-1 ">
-                      {item?.chat_model || '-'}
+                    <p className="text-[10px] text-[#888888] leading-[13px] mt-[3px]">
+                      {item?.model_name || '-'}
                     </p>
                   </div>
                 );
@@ -161,12 +162,12 @@ const SystemNodes: React.FC = () => {
       onCreate={(instance) => setTippyInstance(instance)}
       className="gaianet-tippy"
     >
-      <div className="inline-flex items-center justify-center rounded-lg gap-3 px-3 h-[44px] text-[13px] bg-white text-black border border-[rgba(0, 0, 0, 0.08)] cursor-pointer hover:border-black transition-all">
+      <div className="inline-flex items-center justify-between rounded-lg gap-3 px-3 w-[405px] h-[44px] text-[13px] bg-white text-black border border-[rgba(0, 0, 0, 0.08)] cursor-pointer hover:border-black transition-all">
         <div className="flex flex-col">
           <p className="text-[13px] leading-[16px] ">
             {selectedConversation?.model?.id || defaultModelId}
           </p>
-          <p className="text-[10px] leading-[13px] uppercase mt-[2px]">
+          <p className="text-[10px] leading-[13px] uppercase mt-[2px] text-[#888888]">
             {selectedModelSubdomin || '-'}
           </p>
         </div>
